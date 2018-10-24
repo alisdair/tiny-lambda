02b5dca5da0c1b63a7d25a07e948d0a6b29be031
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2018-10-24 11:05:25 -0400

    New version of the lambda function

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 'use strict';
 
 exports.handler = function (event, context, callback) {
+    var name = event.pathParameters.proxy || "world";
     var response = {
         statusCode: 200,
         headers: {
             'Content-Type': 'text/html; charset=utf-8',
         },
-        body: "<p>Hello world!</p>",
+        body: "<p>Hello, " + name + "!</p>",
     };
     callback(null, response);
 };
