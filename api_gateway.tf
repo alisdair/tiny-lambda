50e501fb045cf8e714b8749f77552b654c792693
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2018-10-24 10:53:41 -0400

    Connect lambda to API gateway

--- api_gateway.tf
+++ api_gateway.tf
@@ -2,3 +2,7 @@ resource "aws_api_gateway_rest_api" "tiny_lambda" {
   name        = "TinyLambda"
   description = "The smallest most pointless lambda function"
 }
+
+output "base_url" {
+  value = "${aws_api_gateway_deployment.example.invoke_url}"
+}
--- lambda.tf
+++ lambda.tf
@@ -88,3 +88,14 @@ resource "aws_api_gateway_deployment" "lambda" {
   rest_api_id = "${aws_api_gateway_rest_api.tiny_lambda.id}"
   stage_name  = "test"
 }
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.tiny_lambda.arn}"
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_api_gateway_deployment.tiny_lambda.execution_arn}/*/*"
+}
