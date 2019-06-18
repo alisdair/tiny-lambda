29c1c9a80c0f8413c6ed6ce107467dd7b4b6f047
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2019-06-18 08:36:35 -0400

    Upgrade to 0.12

--- lambda.tf
+++ lambda.tf
@@ -17,14 +17,13 @@ resource "aws_lambda_function" "tiny_lambda" {
   handler = "main.handler"
   runtime = "nodejs6.10"
 
-  role = "${aws_iam_role.lambda_exec.arn}"
+  role = aws_iam_role.lambda_exec.arn
 }
 
 # IAM role which dictates what other AWS services the Lambda function
 # may access.
 resource "aws_iam_role" "lambda_exec" {
-  name = "serverless_tiny_lambda"
-
+  name               = "serverless_tiny_lambda"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -40,63 +39,64 @@ resource "aws_iam_role" "lambda_exec" {
   ]
 }
 EOF
+
 }
 
 resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = "${aws_api_gateway_rest_api.tiny_lambda.id}"
-  parent_id   = "${aws_api_gateway_rest_api.tiny_lambda.root_resource_id}"
-  path_part   = "{proxy+}"
+  rest_api_id = aws_api_gateway_rest_api.tiny_lambda.id
+  parent_id = aws_api_gateway_rest_api.tiny_lambda.root_resource_id
+  path_part = "{proxy+}"
 }
 
 resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = "${aws_api_gateway_rest_api.tiny_lambda.id}"
-  resource_id   = "${aws_api_gateway_resource.proxy.id}"
-  http_method   = "ANY"
+  rest_api_id = aws_api_gateway_rest_api.tiny_lambda.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = "ANY"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = "${aws_api_gateway_rest_api.tiny_lambda.id}"
-  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.tiny_lambda.id
+  resource_id = aws_api_gateway_method.proxy.resource_id
+  http_method = aws_api_gateway_method.proxy.http_method
 
   integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.tiny_lambda.invoke_arn}"
+  type = "AWS_PROXY"
+  uri = aws_lambda_function.tiny_lambda.invoke_arn
 }
 
 resource "aws_api_gateway_method" "proxy_root" {
-  rest_api_id   = "${aws_api_gateway_rest_api.tiny_lambda.id}"
-  resource_id   = "${aws_api_gateway_rest_api.tiny_lambda.root_resource_id}"
-  http_method   = "ANY"
+  rest_api_id = aws_api_gateway_rest_api.tiny_lambda.id
+  resource_id = aws_api_gateway_rest_api.tiny_lambda.root_resource_id
+  http_method = "ANY"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id = "${aws_api_gateway_rest_api.tiny_lambda.id}"
-  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.tiny_lambda.id
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
 
   integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.tiny_lambda.invoke_arn}"
+  type = "AWS_PROXY"
+  uri = aws_lambda_function.tiny_lambda.invoke_arn
 }
 
 resource "aws_api_gateway_deployment" "lambda" {
   depends_on = [
-    "aws_api_gateway_integration.lambda",
-    "aws_api_gateway_integration.lambda_root",
+    aws_api_gateway_integration.lambda,
+    aws_api_gateway_integration.lambda_root,
   ]
 
-  rest_api_id = "${aws_api_gateway_rest_api.tiny_lambda.id}"
-  stage_name  = "test"
+  rest_api_id = aws_api_gateway_rest_api.tiny_lambda.id
+  stage_name = "test"
 }
 
 resource "aws_lambda_permission" "apigw" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.tiny_lambda.arn}"
-  principal     = "apigateway.amazonaws.com"
+  statement_id = "AllowAPIGatewayInvoke"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.tiny_lambda.arn
+  principal = "apigateway.amazonaws.com"
 
   # The /*/* portion grants access from any method on any resource
   # within the API Gateway "REST API".
